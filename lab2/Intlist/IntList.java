51f9cbf51cdae81302a23095d71e1b35863b473b
brli3 <ruipengyu.li@gmail.com>
2021-08-14 11:41:57 +0100

    null pointer

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,7 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null || B == null) return null;
         IntList p = A;
         while (p.rest != null) p = p.rest;
         p.rest = B;
@@ -94,6 +95,7 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null || B == null) return null;
         IntList C = new IntList(A.first, null);
         IntList p = C;
         A = A.rest;
